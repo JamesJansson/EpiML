3e30451e7d0523a12b603df1afc4fc76f549e9fc
James Jansson <jamesjansson@gmail.com>
2014-07-05 04:06:59 +1000

    Running a function in a class

--- model/person.js
+++ model/person.js
@@ -6,4 +6,11 @@ function  PersonObject(Sex, YearOfBirth)
 	this.YearOfBirth=YearOfBirth;
 	this.YearOfDeath=3E9;
 	this.HCV = new HCVObject(0, 0);
+	
+
 }
+
+PersonObject.prototype.Age = function (Year){
+	return Year-this.YearOfBirth;
+};
+
