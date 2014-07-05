cac9beb5951beccde256dbcc5c7e00beb59bedf3
JamesJansson <jamesjansson@gmail.com>
2014-07-05 18:59:03 +1000

    Creating system

---
+++ interface/initialise.js
@@ -0,0 +1,19 @@
+
+//Testing the required elements for EpiML
+//Web workers
+if(typeof(Worker) == "undefined") {
+alert("Webworkers are not supported in this browser. EpiML requires webworkers to run. Try upgrading your browser");
+}
+//Local storage
+
+
+
+
+
+
+
+
+// scripts 
+var Person = [];//create a global array to store all people
+
+
