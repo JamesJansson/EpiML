0322d667ac7f0ac24226636930f46a21c7294802
James Jansson <jamesjansson@gmail.com>
2014-07-06 03:20:34 +1000

    Fixing main area appearnace

--- model/hcv.js
+++ model/hcv.js
@@ -4,3 +4,7 @@ function  HCVObject(Infected, Diagnosed)
 	this.Diagnosed=Diagnosed;
 	//console.log(this.parentNode);
 }
+
+HCVObject.prototype.Age= function (Year){//using prototyping for speed
+	return Year-this.YearOfBirth;
+};
