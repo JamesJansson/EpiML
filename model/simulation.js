169ebee5346caec74ae671bc394226ea8de458b3
James Jansson <jamesjansson@gmail.com>
2014-07-06 07:25:46 +1000

    Creating time until event

--- model/simulation.js
+++ model/simulation.js
@@ -6,3 +6,30 @@ function  SimObject(SimSize)
 	this.Infected=SimSize;
 	this.Diagnosed=Diagnosed;
 }
+
+
+
+
+
+
+
+
+
+
+// General mathematical functions that are used during simulations
+
+function TimeUntilEvent(Probability)
+{
+	// r=annual probability
+	// p=random number between 0 and 1
+	// p=r^t
+	// p=e^(ln(r)t)
+	// t=ln(p)/ln(r)
+	
+	return (Math.log(Math.random())/Math.log(Probability));
+}
+
+for (var i=1; i<10000; i++)
+{
+	console.log(TimeUntilEvent(0.3));
+}
