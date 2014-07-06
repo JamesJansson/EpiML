d95eab06e3b6051abea0b72daa32e3560533f19d
JamesJansson <jamesjansson@gmail.com>
2014-07-06 23:39:42 +1000

    Extending HCV

--- model/hcv.js
+++ model/hcv.js
@@ -1,4 +1,4 @@
-function  HCVObject()
+function  HCVObject(YearOfBirth)
 {
 	//State variables
 	this.InfectedState=0;
@@ -8,10 +8,30 @@ function  HCVObject()
 	
 	//History variables
 	this.Infected=new EventVector;
+	//this.AntiBody=new EventVector;//?
+	this.Diagnosed=new EventVector;
+	//AntibodyDiagnosis
 	this.Fibrosis=new EventVector;//including liver failure
 	this.HCC=new EventVector;
-
-
+	this.Genotype[0]=new EventVector;
+	this.Genotype[1]=new EventVector;
+	this.Genotype[2]=new EventVector;
+	this.Genotype[3]=new EventVector;
+	this.Genotype[4]=new EventVector;
+	this.Genotype[5]=new EventVector;
+	
+	
+	//Initialise history variables
+	this.Infected.Set(0, YearOfBirth);//note: to set an infection from birth, call HCV.Infection(YearOfBirth,...
+	this.Fibrosis.Set(0, YearOfBirth);
+	this.HCC.Set(0, YearOfBirth);
+	this.Genotype[0].Set(0, YearOfBirth);
+	this.Genotype[1].Set(0, YearOfBirth);
+	this.Genotype[2].Set(0, YearOfBirth);
+	this.Genotype[3].Set(0, YearOfBirth);
+	this.Genotype[4].Set(0, YearOfBirth);
+	this.Genotype[5].Set(0, YearOfBirth);
+	
 }
 
 HCVObject.prototype.Age= function (Year){//using prototyping for speed
@@ -24,14 +44,15 @@ HCVObject.prototype.Infection= function (Year, GenotypeValue, Age, Alcohol, HCVP
 	//This will become especially important in cases where alcoholism begins. In such cases, the remaining time (e.g. 3.5 years to F4) should be shortened accordingly, not recalculated from scratch)
 	
 	this.GenotypeState[GenotypeValue]=1;
+	this.Genotype[GenotypeValue].Set(1, Year);
 	
-	if (this.Infected==0){//if not already infected (don't want to start fibrosis from f1)
+	if (this.InfectedState==0){//if not already infected (don't want to start fibrosis from f1)
 		//State variables
 		this.InfectedState=1;
 		this.AntiBodyState=1;
 		
 		//History variables
-		this.InfectedYear=Year;
+		this.Infected.Set(1, Year);
 		this.AntiBodiesYear=Year;
 		
 		//Determine if spontaneous clearance occurs
@@ -65,7 +86,7 @@ HCVObject.prototype.Infection= function (Year, GenotypeValue, Age, Alcohol, HCVP
 
 HCVObject.prototype.CurrentlyInfected= function (Year){
 
-
+	// rather than doing this, why not simply do Person[i].HCV.Infected.Get(1999)?
 };
 
 
