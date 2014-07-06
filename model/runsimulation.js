63e991e1451eda1fe0f89f51411cdfd516e6b4ba
JamesJansson <jamesjansson@gmail.com>
2014-07-07 02:06:18 +1000

    Trying push for array

--- model/runsimulation.js
+++ model/runsimulation.js
@@ -13,7 +13,7 @@ self.onmessage = function (e) {
 	
 	var PP=[];
 	for (i=0; i<300000; i++){
-		PP[i]=PersonObject(2, 1985.1);
+		PP.Push(new PersonObject(2, 1985.1));
 		if (i%10000==0){
 			self.postMessage({ConsoleMessage: i+" of 300000"});
 		}
