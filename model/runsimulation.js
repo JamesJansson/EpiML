b587ff71a280c5f0818b4e67dc0f8ad9e1bfaf69
JamesJansson <jamesjansson@gmail.com>
2014-07-07 03:42:32 +1000

    Cleaning out code no longer sued

--- model/runsimulation.js
+++ model/runsimulation.js
@@ -4,7 +4,9 @@ importScripts("person.js");
 importScripts("hcv.js");
 
 self.onmessage = function (e) {
-    var SimNumber = e.data.SimNumber;
+    var seconds1 = new Date().getTime() / 1000;
+	
+	var SimNumber = e.data.SimNumber;
     var SimData = e.data.SimData;
 	
 	IncrementSize=Math.round(SimData.NoPeople/100);
@@ -68,23 +70,12 @@ self.onmessage = function (e) {
 	
 	
 	
-	self.postMessage({ConsoleMessage: "Starting to run time until diagnosis"});
 	
-	
-	var Rarray=[];
-	TotalLoops=10000000;
-    var seconds1 = new Date().getTime() / 1000;
-	for (var i=1; i<TotalLoops; i++){
-		Rarray=TimeUntilEvent(0.3);
-		if (i%10000==0){
-			self.postMessage({ProgressBarValue: i/TotalLoops});
-		}
-	}
 	var seconds2 = new Date().getTime() / 1000;
     TotalTime=seconds2 -seconds1;
-	CalcsPerSec=TotalLoops/TotalTime;
+
 	
-	ReturnMessage=WhatsGiven+SimNumber+" "+CalcsPerSec;
 	
-	self.postMessage({ConsoleMessage: "Finished simulation"});
+	
+	self.postMessage({ConsoleMessage: "Finished simulation in "+TotalTime+" seconds"});
 };
