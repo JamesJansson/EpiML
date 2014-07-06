beded5034752376eb630d2a78b25c37c71a07bb6
JamesJansson <jamesjansson@gmail.com>
2014-07-07 04:13:49 +1000

    Trying a new graph

--- model/hcvtestgraph.js
+++ model/hcvtestgraph.js
@@ -1,103 +1,63 @@
-  // source from http://jsfiddle.net/crzyman/nshX6/1/
-  
-  var graph;
-            var xPadding = 30;
-            var yPadding = 30;
-            
-            // Notice I changed The X values
-            var data = { values:[
-                { X: 0, Y: 12 },
-                { X: 2, Y: 28 },
-                { X: 3, Y: 18 },
-                { X: 4, Y: 34 },
-                { X: 5, Y: 40 },
-                { X: 6, Y: 80 },
-                { X: 7, Y: 80 }
-            ]};
-
-            // Returns the max Y value in our data list
-            function getMaxY() {
-                var max = 0;
-                
-                for(var i = 0; i < data.values.length; i ++) {
-                    if(data.values[i].Y > max) {
-                        max = data.values[i].Y;
-                    }
-                }
-                
-                max += 10 - max % 10;
-                return max;
-            }
-
-            // Returns the max X value in our data list
-            function getMaxX() {
-                var max = 0;
-                
-                for(var i = 0; i < data.values.length; i ++) {
-                    if(data.values[i].X > max) {
-                        max = data.values[i].X;
-                    }
-                }
-                
-                max += 10 - max % 10;
-                return max;
-            }
-            
-            // Return the x pixel for a graph point
-            function getXPixel(val) {
-                // uses the getMaxX() function
-                return ((graph.width - xPadding) / getMaxX()) * val + (xPadding * 1.5);
-            }
-            
-            // Return the y pixel for a graph point
-            function getYPixel(val) {
-                return graph.height - (((graph.height - yPadding) / getMaxY()) * val) - yPadding;
-            }
-
-                graph = document.getElementById("graph");
-                var c = graph.getContext('2d');            
-                
-                c.lineWidth = 2;
-                c.strokeStyle = '#333';
-                c.font = 'italic 8pt sans-serif';
-                c.textAlign = "center";
-                
-                // Draw the axises
-                c.beginPath();
-                c.moveTo(xPadding, 0);
-                c.lineTo(xPadding, graph.height - yPadding);
-                c.lineTo(graph.width, graph.height - yPadding);
-                c.stroke();
-                
-                // Draw the X value texts
-                for(var i = 0; i < data.values.length; i ++) {
-                    // uses data.values[i].X
-                    c.fillText(data.values[i].X, getXPixel(data.values[i].X), graph.height - yPadding + 20);
-                }
-                
-                // Draw the Y value texts
-                c.textAlign = "right"
-                c.textBaseline = "middle";
-                
-                for(var i = 0; i < getMaxY(); i += 10) {
-                    c.fillText(i, xPadding - 10, getYPixel(i));
-                }
-                
-                c.strokeStyle = '#f00';
-                
-                // Draw the line graph
-                c.beginPath();
-                c.moveTo(getXPixel(data.values[0].X), getYPixel(data.values[0].Y));
-                for(var i = 1; i < data.values.length; i ++) {
-                    c.lineTo(getXPixel(data.values[i].X), getYPixel(data.values[i].Y));
-                }
-                c.stroke();
-                
-                // Draw the dots
-                c.fillStyle = '#333';
-                
-                for(var i = 0; i < data.values.length; i ++) {  
-                    c.beginPath();
-                    c.arc(getXPixel(data.values[i].X), getYPixel(data.values[i].Y), 4, 0, Math.PI * 2, true);
-                    c.fill();
-                }
+		//https://gist.github.com/benjchristensen/2579599
+		/* implementation heavily influenced by http://bl.ocks.org/1166403 */
+		
+		// define dimensions of graph
+		var m = [80, 80, 80, 80]; // margins
+		var w = 1000 - m[1] - m[3]; // width
+		var h = 400 - m[0] - m[2]; // height
+		
+		// create a simple data array that we'll plot with a line (this array represents only the Y values, X will just be the index location)
+		var data = [3, 6, 2, 7, 5, 2, 0, 3, 8, 9, 2, 5, 9, 3, 6, 3, 6, 2, 7, 5, 2, 1, 3, 8, 9, 2, 5, 9, 2, 7];
+ 
+		// X scale will fit all values from data[] within pixels 0-w
+		var x = d3.scale.linear().domain([0, data.length]).range([0, w]);
+		// Y scale will fit values from 0-10 within pixels h-0 (Note the inverted domain for the y-scale: bigger is up!)
+		var y = d3.scale.linear().domain([0, 10]).range([h, 0]);
+			// automatically determining max range can work something like this
+			// var y = d3.scale.linear().domain([0, d3.max(data)]).range([h, 0]);
+ 
+		// create a line function that can convert data[] into x and y points
+		var line = d3.svg.line()
+			// assign the X function to plot our line as we wish
+			.x(function(d,i) { 
+				// verbose logging to show what's actually being done
+				console.log('Plotting X value for data point: ' + d + ' using index: ' + i + ' to be at: ' + x(i) + ' using our xScale.');
+				// return the X coordinate where we want to plot this datapoint
+				return x(i); 
+			})
+			.y(function(d) { 
+				// verbose logging to show what's actually being done
+				console.log('Plotting Y value for data point: ' + d + ' to be at: ' + y(d) + " using our yScale.");
+				// return the Y coordinate where we want to plot this datapoint
+				return y(d); 
+			})
+ 
+			// Add an SVG element with the desired dimensions and margin.
+			var graph = d3.select("#graph").append("svg:svg")
+			      .attr("width", w + m[1] + m[3])
+			      .attr("height", h + m[0] + m[2])
+			    .append("svg:g")
+			      .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
+ 
+			// create yAxis
+			var xAxis = d3.svg.axis().scale(x).tickSize(-h).tickSubdivide(true);
+			// Add the x-axis.
+			graph.append("svg:g")
+			      .attr("class", "x axis")
+			      .attr("transform", "translate(0," + h + ")")
+			      .call(xAxis);
+ 
+ 
+			// create left yAxis
+			var yAxisLeft = d3.svg.axis().scale(y).ticks(4).orient("left");
+			// Add the y-axis to the left
+			graph.append("svg:g")
+			      .attr("class", "y axis")
+			      .attr("transform", "translate(-25,0)")
+			      .call(yAxisLeft);
+			
+  			// Add the line by appending an svg:path element with the data line we created above
+			// do this AFTER the axes above so that the line is above the tick-lines
+  			graph.append("svg:path").attr("d", line(data));
+			
+ 
