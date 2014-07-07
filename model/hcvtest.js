ad9bd8fa10ce2b00ed479913d014444c75aaeaa0
JamesJansson <jamesjansson@gmail.com>
2014-07-07 17:25:58 +1000

    Removing old code

--- model/hcvtest.js
+++ model/hcvtest.js
@@ -96,21 +96,7 @@ function HCVTestPlot(){
 	
 	//plot of HCVTestPlotHolder
 	$.plot("#HCVTestPlotHolder", PlotData, PlotSettings);
-	
-	
-	//var d1 = [];
-	//for (var i = 0; i < 14; i += 0.5) {
-	//	d1.push([i, Math.sin(i)]);
-	//}
 
-	//var d2 = [[0, 3], [4, 8], [8, 5], [9, 13]];
-
-	// A null signifies separate line segments
-
-	//var d3 = [[0, 12], [7, 12], null, [7, 2.5], [12, 2.5]];
-
-	//$.plot("#HCVTestPlotHolder", [ d1, d2, d3 ]);
-	//console.log([d1, d2, d3 ]);
 }
 
 function ConvertDataToLinePlot(x, InputMatrix){//Accepts [param][time] or [y][x]. Future systems will accept [param][time][sim]
