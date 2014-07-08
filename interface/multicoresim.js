f15f1097ac60c109380faeafcc9c71a657d1ec71
JamesJansson <jamesjansson@gmail.com>
2014-07-08 22:34:35 +1000

    Currying function

--- interface/multicoresim.js
+++ interface/multicoresim.js
@@ -84,7 +84,9 @@ MulticoreSim.prototype.RampUpSims=function() {
 			console.log("Started sim: "+SimID);
 			
 			this.Worker= new Worker(this.ScriptName);
-			this.Worker.onmessage = function (e){MulticoreSimMessageHandler(e, this)};
+			var BoundHandler = MulticoreSimMessageHandler(this, null);
+			this.Worker.onmessage = BoundHandler;
+			//this.Worker.onmessage = function (e){MulticoreSimMessageHandler(e, this)};
 			this.Worker.postMessage({ SimNumber: SimID, CommonData: this.CommonData, SimData: this.SimDataArray[SimID]});
 			
 		}
@@ -97,7 +99,7 @@ MulticoreSim.prototype.RampUpSims=function() {
 }
 
 
-MulticoreSimMessageHandler=function(e, MulticoreSimObject) {
+MulticoreSimMessageHandler=function(MulticoreSimObject, e) {
 	// There are 3 main message types that are handled
 	// Messages to the StatusText (to be put somewhere on screen to indicate what is currently occurring)
 	// Messages to the ProgressBar
