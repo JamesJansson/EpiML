8b260f366c8f5f827ab1c180172448d50d2fa1ff
JamesJansson <jamesjansson@gmail.com>
2014-07-08 23:37:05 +1000

    Creating a testing script

---
+++ test/testmulticoresim.js
@@ -0,0 +1,13 @@
+function TestMulticoreSim(){
+
+ScriptToRun='model/runsimulation.js';
+SimData=[];
+for (i=0; i<10; i++){
+	SimData[i]={};
+	SimData[i].NoPeople=10000;
+}
+Cores=3;
+SimulationObject=new MulticoreSim(ScriptToRun, 22, SimData, Cores); 
+SimulationObject.Start();
+return 0;
+}
