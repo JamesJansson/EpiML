e137487e0a4b7cf48ab38adc23d2f855bb95f78e
JamesJansson <jamesjansson@gmail.com>
2014-07-08 23:52:51 +1000

    Make it big

--- test/testmulticoresim.js
+++ test/testmulticoresim.js
@@ -6,7 +6,7 @@ ScriptToRun='model/runsimulation.js';
 SimData=[];
 for (i=0; i<10; i++){
 	SimData[i]={};
-	SimData[i].NoPeople=10000;
+	SimData[i].NoPeople=100000;
 }
 Cores=3;
 SimulationObject=new MulticoreSim(ScriptToRun, 22, SimData, Cores); 
