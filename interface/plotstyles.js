6ce6fc426c5a259c0b3395db1a031cc21037d518
JamesJansson <jamesjansson@gmail.com>
2014-08-04 14:24:53 +1000

    Making graphs look good

--- interface/plotstyles.js
+++ interface/plotstyles.js
@@ -1,5 +1,22 @@
-function StackedPlot(PlotHolderName, X, ArrayToPlot,  xAxisLabel, yAxisLabel){
+// Developed by James Jansson
+// Requires the flot plotting library to work
 
+
+function ConvertDataToLinePlot(x, InputMatrix){//Accepts [param][time] or [y][x]. Future systems will accept [param][time][sim]
+	LinePlotData=[];
+	NumberOfLines=InputMatrix.length;
+	for (i=0; i<NumberOfLines; i++){
+		ThisLine=[];
+		for (var j = 0; j < InputMatrix[i].length; j++) {
+			ThisLine.push([x[j], InputMatrix[i][j]]);
+		}
+		LinePlotData.push(ThisLine);
+	}
+	return LinePlotData;
+}
+
+
+function StackedLinePlot(PlotHolderName, X, ArrayToPlot,  xAxisLabel, yAxisLabel){
 	// convert to a form that plot will accept
 	PlotData=ConvertDataToLinePlot(X, ArrayToPlot);
 	//Set up plot appearance // http://www.pikemere.co.uk/blog/flot-tutorial-how-to-create-area-charts/ 
@@ -26,22 +43,39 @@ function StackedPlot(PlotHolderName, X, ArrayToPlot,  xAxisLabel, yAxisLabel){
 					stack: true
 				}
 			};
-	
-	//plot of HCVTestPlotHolder
 	$.plot(PlotHolderName, PlotData, PlotSettings);
+}
 
+function StackedBarPlot(PlotHolderName, X, ArrayToPlot,  xAxisLabel, yAxisLabel){
+	// convert to a form that plot will accept
+	PlotData=ConvertDataToLinePlot(X, ArrayToPlot);
+	//Set up plot appearance // http://www.pikemere.co.uk/blog/flot-tutorial-how-to-create-area-charts/ 
+	PlotSettings={xaxis: {
+					axisLabel: xAxisLabel,
+					axisLabelUseCanvas: true,
+					axisLabelFontSizePixels: 12,
+					axisLabelFontFamily: 'Verdana, Arial, Helvetica, Tahoma, sans-serif',
+					axisLabelPadding: 5,
+					tickLength: 0
+				},
+				yaxis: {
+					axisLabel: yAxisLabel,
+					axisLabelUseCanvas: true,
+					axisLabelFontSizePixels: 12,
+					axisLabelFontFamily: 'Verdana, Arial, Helvetica, Tahoma, sans-serif',
+					axisLabelPadding: 5
+				},
+				series: {
+					bars: {
+						align: "center",
+						show: true,
+						barWidth: 0.6,
+						fill: true
+					},
+					stack: true
+				}
+			};
+	$.plot(PlotHolderName, PlotData, PlotSettings);
 }
 
 
-function ConvertDataToLinePlot(x, InputMatrix){//Accepts [param][time] or [y][x]. Future systems will accept [param][time][sim]
-	LinePlotData=[];
-	NumberOfLines=InputMatrix.length;
-	for (i=0; i<NumberOfLines; i++){
-		ThisLine=[];
-		for (var j = 0; j < InputMatrix[i].length; j++) {
-			ThisLine.push([x[j], InputMatrix[i][j]]);
-		}
-		LinePlotData.push(ThisLine);
-	}
-	return LinePlotData;
-}
