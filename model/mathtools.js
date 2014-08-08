125f2d4b9a5ffab5f22f50897541ee36786f2d54
JamesJansson <jamesjansson@gmail.com>
2014-08-08 19:49:22 +1000

    Significant updates to mathtools

--- model/mathtools.js
+++ model/mathtools.js
@@ -1,10 +1,131 @@
+// Mathtools
+// Mathtools is the mathematicians' lazy library for javascript. The library operates on simple arrays.
+// This library breaks a lot of the standards of javascript, but is intended to make for easily read code from a mathematician's perspective.
+// This library copies code from jstat https://github.com/jstat/jstat
+// Code is especially ripped from https://github.com/jstat/jstat/blob/master/src/vector.js
+// As such, this library is licensed using the MIT license 
+
+// Sum of an array
+function Sum(arr) {
+  var sum = 0;
+  var i = arr.length;
+  var tmp;
+  while (--i >= 0)
+    sum += arr[i];
+  return sum;
+}
+
+// Mean value of an array
+function Mean(arr) {
+  return Sum(arr) / arr.length;
+}
+
+// Median of an array
+function Median(MInputVector){
+	VectorCopy=MInputVector.slice();
+	SortNumerically(VectorCopy);
+	VecLength=VectorCopy.length;
+	if ((VecLength%2)==1){//odd number of elements
+		return VectorCopy[(VecLength-1)/2];//chose the middle value
+	}
+	else{
+		return (VectorCopy[(VecLength)/2]+VectorCopy[(VecLength)/2-1])/2;
+	}
+}
+
+function CumSum(arr) {
+  var len = arr.length;
+  var sums = new Array(len);
+  var i;
+  sums[0] = arr[0];
+  for (i = 1; i < len; i++)
+    sums[i] = sums[i - 1] + arr[i];
+  return sums;
+}
+
+
+// minimum value of an array
+function Min(arr) {
+  var low = arr[0];
+  var i = 0;
+  while (++i < arr.length)
+    if (arr[i] < low)
+      low = arr[i];
+  return low;
+}
+
+// maximum value of an array
+function Max(arr) {
+  var high = arr[0];
+  var i = 0;
+  while (++i < arr.length)
+    if (arr[i] > high)
+      high = arr[i];
+  return high;
+}
+
+/*
+// sum of squared errors of prediction (SSE)
+jStat.sumsqerr = function sumsqerr(arr) {
+  var mean = jStat.mean(arr);
+  var sum = 0;
+  var i = arr.length;
+  var tmp;
+  while (--i >= 0) {
+    tmp = arr[i] - mean;
+    sum += tmp * tmp;
+  }
+  return sum;
+};
+*/
+
+
+/*
+// variance of an array
+// flag indicates population vs sample
+jStat.variance = function variance(arr, flag) {
+  return jStat.sumsqerr(arr) / (arr.length - (flag ? 1 : 0));
+};
+
+
+// standard deviation of an array
+// flag indicates population vs sample
+jStat.stdev = function stdev(arr, flag) {
+  return Math.sqrt(jStat.variance(arr, flag));
+};*/
+
+/*
+// covariance of two arrays
+jStat.covariance = function covariance(arr1, arr2) {
+  var u = jStat.mean(arr1);
+  var v = jStat.mean(arr2);
+  var arr1Len = arr1.length;
+  var sq_dev = new Array(arr1Len);
+  var i;
+
+  for (i = 0; i < arr1Len; i++)
+    sq_dev[i] = (arr1[i] - u) * (arr2[i] - v);
+
+  return jStat.sum(sq_dev) / (arr1Len - 1);
+};
+
+
+// (pearson's) population correlation coefficient, rho
+jStat.corrcoeff = function corrcoeff(arr1, arr2) {
+  return jStat.covariance(arr1, arr2) /
+      jStat.stdev(arr1, 1) /
+      jStat.stdev(arr2, 1);
+};*/
+
+
+
+
 
 // Sort numerically
 function SortNumerically(SNInputVector){
 	// this function sorts the vector as given and returns the index of sorting
 	SNInputVector.sort(function(a, b) {return a - b;});
-		//var b = a.slice();
-
+	//return index
 }
 
 function ApplyIndex(AIInputVector, AIIndex){
@@ -25,6 +146,8 @@ function ApplyIndex(AIInputVector, AIIndex){
 
 
 
+
+
 //Create an array of values
 function AscendingArray(StartValue, EndValue, IncrementBy){
     ReturnArray=[];
@@ -35,6 +158,14 @@ function AscendingArray(StartValue, EndValue, IncrementBy){
     return ReturnArray;
 }
 
+//Create an array of zeros
+function ZeroArray(Length){
+    ReturnArray=[];
+	for (i=0; i<Length; i++){
+		ReturnArray[i]=0;
+	}
+    return ReturnArray;
+}
 
 
 // Zero matrix
