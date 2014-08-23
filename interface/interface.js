dde926d5170053aa6d7eca4523177df1d3d2665b
JamesJansson <jamesjansson@gmail.com>
2014-08-24 00:11:04 +1000

    Creating AddNewParameter

--- interface/interface.js
+++ interface/interface.js
@@ -3,7 +3,7 @@
 //   put data in <div id="HCVParams">
 //	 for each parameter in each subgroup
 
-function LoadParameterPages(ParamGroup, ParamGroupDivId){
+function BuildParameterPage(ParamGroup, ParamGroupDivId){
 	var BuildText="";
 	for (var key in ParamGroup) {// Code inspired by http://stackoverflow.com/questions/208016/how-to-list-the-properties-of-a-javascript-object
 		if (ParamGroup.hasOwnProperty(key)) {
@@ -25,9 +25,17 @@ function LoadParameterPages(ParamGroup, ParamGroupDivId){
 
 
 function AddNewParameter(ParamGroup, ParamGroupDivId){
+	//Count number of current 
+	var NumParams=1;
+	for (var key in ParamGroup) {
+		NumParams++;
+	}
+	var ParamName="NewParam"+NumParams;//The new parameter will be called e.g. NewParam12
+	ParamGroup[ParamName]=new ParameterClass(ParamName);
+	// Name the interface
+	ParamGroup[ParamName].InterfaceID=ParamGroupDivId+ParamName;
+	document.getElementById(ParamGroupDivId).innerHTML = document.getElementById(ParamGroupDivId).innerHTML + ParamGroup[ParamName].InterfaceID;
 	
-
-
 }
 
 
