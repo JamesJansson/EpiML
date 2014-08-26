a28b08b49d14151b3eff283efee9bc673cf1b93c
JamesJansson <jamesjansson@gmail.com>
2014-08-26 21:09:33 +1000

    Cleaning up of code

--- model/assignpopulation.js
+++ model/assignpopulation.js
@@ -4,13 +4,8 @@ function AssignPopulation(Data, Param){
 	var NumAgeGroups=Data.MaleNotifications.Age.length;
 	var AgeVector=Data.MaleNotifications.Age;//This +5 is the range
 	
-	
-	//Param.SampleFactor
+	console.log("Warning: duplicate rate reset here.");
 	Param.DuplicateFactor=0;// maybe a per year by age probability, 
-	
-	
-	
-	//var StateNotificationCount=Data.StateNotifications.slice();//copy state notifications, because we plan count down the number of 
 	
 	var StateVector;
 	var TotalInStateThisYear;
@@ -32,8 +27,6 @@ function AssignPopulation(Data, Param){
 	for (var YearIndex=0; YearIndex<Data.StateNotifications.Year.length; YearIndex++){
 		YearThisStep=Data.StateNotifications.Year[YearIndex];
 		
-		console.log("Starting year "+YearThisStep);
-		
 		StateVector=[];//reset
 		TotalInStateThisYear=0;
 		for (var StateIndex=0; StateIndex<Data.StateNotifications.State.length; StateIndex++){
@@ -48,20 +41,12 @@ function AssignPopulation(Data, Param){
 			}
 		}
 		StateVector=Shuffle(StateVector);
-			
-		console.log("State vector length "+StateVector.length);
-
 		
 		// Later: for each state create a vector of 
-		// aboriginality [0, 0, 1, 1, 0, 0...]
+		// Aboriginality [0, 0, 1, 1, 0, 0...]
 		// HIV coinfection
 		
 		// Alcoholism
-		
-		
-		
-		
-		
 		
 		// For each age group
 		for (var AgeIndex=0; AgeIndex<NumAgeGroups; AgeIndex++){
@@ -74,20 +59,16 @@ function AssignPopulation(Data, Param){
 				//To compensate, the algorithm adds a person with a probability based on the remainder
 				// e.g. and entry with 3.4 people will give 3 people plus one extra person with 40% probability 
 				EstimateInThisGroup=EstimateInThisGroup*(1-Param.DuplicateFactor)/Param.SampleFactor;
-				console.log("Param.SampleFactor "+ Param.SampleFactor);
 				EstimateInThisGroup1=Math.floor(EstimateInThisGroup);
-				console.log("EstimateInThisGroup1 "+ EstimateInThisGroup1);
 				EstimateInThisGroup2=EstimateInThisGroup-EstimateInThisGroup1;// Some value between 0 and 1
-				console.log("EstimateInThisGroup2 "+ EstimateInThisGroup2);
 				InThisGroup=EstimateInThisGroup1+Win(EstimateInThisGroup2);
-				
-				console.log("In this group "+ InThisGroup);
 				
 				for (var CountInThisGroup=0; CountInThisGroup<InThisGroup; CountInThisGroup++){
 					YearOfDiagnosis=YearThisStep+Rand.Value();
 					AgeAtDiagnosis=AgeVector[AgeIndex]+5*Rand.Value();
 					YearOfBirth=YearOfDiagnosis-AgeAtDiagnosis;
 					
+					// Create the new person
 					PNotification[PCount]=new PersonObject(YearOfBirth, SexIndex);
 					
 					// Set the diagnosis date for the individual
@@ -95,21 +76,13 @@ function AssignPopulation(Data, Param){
 					
 					//Set the state value
 					PNotification[PCount].Location.Set(StateVector[PCount], YearOfBirth);
-					
 					PCount++;
 				}
 			}
-			console.log("Total people "+ PCount);
 		}
-		// Choose the state from the remaining population
-		
-		
-		
-		
-		
 	}
 
-	console.log("Total people "+ PCount);
+	console.log("Total notifications loaded "+ PCount);
 
 	return PNotification;
 }
