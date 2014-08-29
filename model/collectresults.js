a29eb6bf65ca1dde101a10e3aa44e42bb7757e28
JamesJansson <jamesjansson@gmail.com>
2014-08-29 14:44:15 +1000

    Improving summary statistics

--- model/collectresults.js
+++ model/collectresults.js
@@ -33,10 +33,15 @@ function DetermineTotalHCCInfected(PPLocal){
 	var FibrosisMatrix=new ZeroMatrix(6, StopYear-StartYear+1);
 
 	CategoricalValueFunction=function (Person, Year){
-		return Person.HCV.Fibrosis.Value(Year);
+		return Person.HCV.Fibrosis.Value(Year);// in this case, the returned value is the numerical value found in the 
 	};
 	// can we make this an array of categorical value functions like []
-	
+	// MultiplePopulations
+	// Single population
+	// Events
+	// First events
+	// All events
+	// Status
 	
 
 	var YearCount=0;
@@ -51,3 +56,17 @@ function DetermineTotalHCCInfected(PPLocal){
 	
 	return FibrosisMatrix;
 }
+
+function SummaryStatistic(Type){
+	var CategoricalValueFunction;
+	var SelectionFunction;
+	var SelectionFunctionArray=[];
+	var StartYear;
+	var StopYear;
+	
+	
+}
+
+
+
+// Adjust: multiply by a factor to accommodate for using a reprentative sample, for example
