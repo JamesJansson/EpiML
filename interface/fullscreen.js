cc3410e5da3e8fd833ba036f27056c11be91238c
James Jansson <jamesjansson@gmail.com>
2014-11-29 23:26:20 +1100

    minor

--- interface/fullscreen.js
+++ interface/fullscreen.js
@@ -4,18 +4,18 @@
 // All sub divs should use 100% of the space, such that when full screen occurs, the sub divs expand appropriately.
 
 function GoFullScreen (ElementID){
-var i = document.getElementById(ElementID);
- 
-// go full-screen
-if (typeof(i.requestFullscreen) != "undefined") {
-    i.requestFullscreen();
-} else if (typeof(i.webkitRequestFullscreen) != "undefined") {
-	i.webkitRequestFullscreen();
-} else if (typeof(i.mozRequestFullScreen) != "undefined") {
-	i.mozRequestFullScreen();
-} else if (typeof(i.msRequestFullscreen) != "undefined") {
-	i.msRequestFullscreen();
-}
+	var i = document.getElementById(ElementID);
+	 
+	// go full-screen
+	if (typeof(i.requestFullscreen) != "undefined") {
+		i.requestFullscreen();
+	} else if (typeof(i.webkitRequestFullscreen) != "undefined") {
+		i.webkitRequestFullscreen();
+	} else if (typeof(i.mozRequestFullScreen) != "undefined") {
+		i.mozRequestFullScreen();
+	} else if (typeof(i.msRequestFullscreen) != "undefined") {
+		i.msRequestFullscreen();
+	}
 }
 
 
