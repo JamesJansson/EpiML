20fddd2fd6e20895c3edd41e2810cfc54fa4b1a2
James Jansson <jamesjansson@gmail.com>
2015-02-06 14:46:50 +1100

    Adding download stuff

--- interface/interfacebuttons.js
+++ interface/interfacebuttons.js
@@ -5,3 +5,14 @@ function SaveParameters(){
 	var blob = new Blob([ParamJSONString], {type: "text/plain;charset=utf-8"});
 	saveAs(blob, "parameters.json");
 }
+
+function DownloadSimulationResults (){
+	// Join the parameters, optimised parameters and simulations results into one.
+	// Conver to JSON
+	// Download results
+}
+
+function LoadSimulationResults(){
+	// The purpose of this function is to open a file of previously run results 
+	// This is either to run the simulation again with a change of parameters, or to inspect the results in the file.
+}
