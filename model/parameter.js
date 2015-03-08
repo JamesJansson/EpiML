78c1542429878ab8342cef6ee85fbd50fb8b61aa
James Jansson <jamesjansson@gmail.com>
2015-03-08 13:48:44 +1100

    Fixed distribution bug

--- model/parameter.js
+++ model/parameter.js
@@ -248,7 +248,7 @@ ParameterClass.prototype.CreateDistribution= function (){
 		this.Val=NormalRandArray(LogMedian, this.StandardError, this.NumberOfSamples);
 		this.Val=Exp(this.Val);
 	}
-	if (this.DistributionType=="lognormal"){
+	if (this.DistributionType=="uniform"){
 		this.Val=RandArray(this.LowerBound, this.UpperBound, this.NumberOfSamples);
 	}
 	
