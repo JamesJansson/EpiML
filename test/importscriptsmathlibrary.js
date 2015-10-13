6e4bf4e147241192f51c66102df6defbc652f33e
James Jansson <jamesjansson@gmail.com>
2015-10-13 18:08:12 +1100

    Did a big fix to importScripts

---
+++ test/importscriptsmathlibrary.js
@@ -0,0 +1,19 @@
+function Add(a, b){
+	return a+b;
+}
+
+function Minus(a, b){
+	return a-b;
+}
+
+function MathSendConsoleLog(text){
+	console.log(text);
+}
+
+function MathSendConsoleError(text){
+	console.error(text);
+}
+
+function MathSendThrow(text){
+	throw text;
+}
